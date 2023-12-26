cb5915d33c0927b05a015c5ca2f632bc991ec929
Olivier <bourgeoisolivier@hotmail.fr>
2023-12-26 12:18:24 +0100

    8-Spring Boot Rest API - API Documentation

--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/SpringbootRestfulWebservicesApplication.java
@@ -5,7 +5,33 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring Boot REST API Documentation",
+				description = "",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Ramesh",
+						email = "javaguides.net@gmail.com",
+						url = "https://www.javaguides.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.javaguides.net/license"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Spring Boot User Managment Documentation",
+				url = "https://www.javaguides.net/user_management.html"
+		)
+)
 public class SpringbootRestfulWebservicesApplication {
 
 	@Bean
--- springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
+++ springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -1,12 +1,10 @@
 package net.javaguides.springboot.controller;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,15 +12,19 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.WebRequest;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
-import net.javaguides.springboot.exception.ErrorDetails;
-import net.javaguides.springboot.exception.ResourceNotFoundException;
 import net.javaguides.springboot.service.UserService;
 
+@Tag(
+		name = "CRUD RESt APIs for User Resource",
+		description = "Create User APIs - Update User, Get User, Get All Users, Delete User"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/users")
@@ -32,6 +34,14 @@ public class UserController {
 	
 	// Build create User REST API
 	// http://localhost:8080/api/users
+	@Operation(
+			summary = "Create User REST API",
+			description = "Create User REST API is used to save in a database"
+	)
+	@ApiResponse(
+			responseCode = "201",
+			description = "HTTP Status 201 CREATED"
+	)
 	@PostMapping
 	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
 		UserDto savedUser = userService.createUser(user);
@@ -40,6 +50,14 @@ public class UserController {
 	
 	// Build get user by id REST API
 	// http://localhost:8080/api/users/1
+	@Operation(
+			summary = "Get User By Id REST API",
+			description = "Get User By Id REST API is used to get all users from the database"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+	)
 	@GetMapping("{id}")
 	public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId) {
 		UserDto user = userService.getUserById(userId);
@@ -48,6 +66,14 @@ public class UserController {
 	
 	// Build Get All Users Rest Api
 	// http://localhost:8080/api/users
+	@Operation(
+			summary = "Get All Users REST API",
+			description = "Get All Users REST API is used to get a single user from the database"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+	)
 	@GetMapping
 	public ResponseEntity<List<UserDto>> getAllUsers() {
 		List<UserDto> users = userService.getAllUsers();
@@ -56,6 +82,14 @@ public class UserController {
 	
 	// Build Updated User REST API
 	// http//localhost:8080/api/users/1
+	@Operation(
+			summary = "Update User REST API",
+			description = "Update User REST API is used to update a particular user in the database"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+	)
 	@PutMapping("{id}")
 	public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId, @RequestBody @Valid UserDto user) {
 		user.setId(userId);
@@ -65,6 +99,14 @@ public class UserController {
 	
 	// Build Delete User REST API
 	// http://localhost:8080/api/users/1
+	@Operation(
+			summary = "Delete User REST API",
+			description = "Delete User REST API is used to delete a particular user from the database"
+	)
+	@ApiResponse(
+			responseCode = "200",
+			description = "HTTP Status 200 SUCCESS"
+	)
 	@DeleteMapping("{id}")
 	public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId) {
 		userService.deleteUser(userId);
