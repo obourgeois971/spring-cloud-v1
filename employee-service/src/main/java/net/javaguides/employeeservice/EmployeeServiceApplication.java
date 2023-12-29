8bd77cf13cc73d0b08a101e70a2a4c2b01d4598d
Olivier <bourgeoisolivier@hotmail.fr>
2023-12-29 23:11:05 +0100

    12-API Gateway using Spring Cloud Gateway

--- employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -2,18 +2,21 @@ package net.javaguides.employeeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableFeignClients
 // @EnableEurekaClient
 public class EmployeeServiceApplication {
 
-	// @Bean
-	// @LoadBalanced
-	// public RestTemplate restTemplate() {
-		// return new RestTemplate();
-	// }
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	// @Bean
 	// public WebClient webClient() {
