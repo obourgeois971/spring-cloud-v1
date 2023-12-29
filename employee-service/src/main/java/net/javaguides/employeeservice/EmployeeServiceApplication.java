10a40cce7eed5c9616eae8026c190e30293797ae
Olivier <bourgeoisolivier@hotmail.fr>
2023-12-29 19:52:50 +0100

    11-Service Registry and Discovery using Spring Boot Cloud Netflix Eureka

--- employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-//@EnableEurekaClient
+// @EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	// @Bean
--- employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/APIClient.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import net.javaguides.employeeservice.dto.DepartmentDto;
 
 // Test Get: http://localhost:8081/api/employees/3
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+// 1. @FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+// 2.
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
 	// Build get depratment rest api
 	@GetMapping("api/departments/{department-code}")
