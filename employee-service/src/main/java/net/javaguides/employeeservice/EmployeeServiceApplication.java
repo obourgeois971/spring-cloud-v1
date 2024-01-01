13e55a26da0b9922e5d2668f8e00d8667e1a1db6
Olivier <bourgeoisolivier@hotmail.fr>
2024-01-01 23:02:49 +0100

    16-Circuit Breaker using Resilience4J Implementation

--- employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/EmployeeServiceApplication.java
@@ -6,22 +6,23 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableFeignClients
 // @EnableEurekaClient
 public class EmployeeServiceApplication {
 
-	@Bean
-	@LoadBalanced
-	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+//	@Bean
+//	@LoadBalanced
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
 
-	// @Bean
-	// public WebClient webClient() {
-		// return WebClient.builder().build();
-	// }
+	@Bean
+	public WebClient webClient() {
+		return WebClient.builder().build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeServiceApplication.class, args);
--- employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/net/javaguides/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -1,9 +1,14 @@
 package net.javaguides.employeeservice.service.impl;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import net.javaguides.employeeservice.dto.APIResponseDto;
 import net.javaguides.employeeservice.dto.DepartmentDto;
 import net.javaguides.employeeservice.dto.EmployeeDto;
@@ -14,16 +19,20 @@ import net.javaguides.employeeservice.service.EmployeeService;
 
 @Service
 @AllArgsConstructor
+// @Slf4j
 public class EmployeeServiceImpl implements EmployeeService {
 
+	// private Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
+	
 	private EmployeeRepository employeeRepository;
 	
 	// 1. private RestTemplate restTemplate;
 	
-	// 2. private WebClient webClient;
+	// 2. 
+	private WebClient webClient;
 	
 	// 3.
-	private APIClient apiClient;
+	// private APIClient apiClient;
 
 	@Override
 	public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -50,8 +59,13 @@ public class EmployeeServiceImpl implements EmployeeService {
 	}
 
 	@Override
+	// 1. @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
+	@Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultDepartment")
 	// Test with : http://localhost:8081/api/employees/3 get
 	public APIResponseDto getEmployeeById(Long employeeId) {
+		
+		// LOGGER.info("inside getEmployeeById() method");
+		
 		Employee employee = employeeRepository.findById(employeeId).get();
 		
 		/* 1. ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
@@ -61,14 +75,41 @@ public class EmployeeServiceImpl implements EmployeeService {
 
 		// 1. DepartmentDto departmentDto = responseEntity.getBody();
 
-		/* 2. DepartmentDto departmentDto = webClient.get()
+		// 2. 
+		DepartmentDto departmentDto = webClient.get()
 				 .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
 				 .retrieve()
 				 .bodyToMono(DepartmentDto.class)
-				 .block();*/
+				 .block();
 		
 		// 3.
-		DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+		// DepartmentDto departmentDto = apiClient.getDepartment(employee.getDepartmentCode());
+		
+		EmployeeDto employeeDto = new EmployeeDto(
+				employee.getId(),
+				employee.getFirstName(),
+				employee.getLastName(),
+				employee.getEmail(),
+				employee.getDepartmentCode()
+		);
+		
+		APIResponseDto apiResponseDto = new APIResponseDto();
+		apiResponseDto.setEmployee(employeeDto);
+		apiResponseDto.setDepartment(departmentDto);
+		
+		return apiResponseDto;
+	}
+	
+	public APIResponseDto getDefaultDepartment(Long employeeId, Exception exception) {
+		
+		// LOGGER.info("inside getDefaultDepartment() method");
+		
+		Employee employee = employeeRepository.findById(employeeId).get();
+		
+		DepartmentDto departmentDto = new DepartmentDto();
+		departmentDto.setDepartmentName("R&D Department");
+		departmentDto.setDepartmentDescription("Research and Development Department");
+		departmentDto.setDepartmentCode("RD001");
 		
 		EmployeeDto employeeDto = new EmployeeDto(
 				employee.getId(),
