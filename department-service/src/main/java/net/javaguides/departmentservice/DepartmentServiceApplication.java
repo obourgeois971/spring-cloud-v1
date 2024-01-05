5a25c863993d5268140f1d399ec574c68261499f
Olivier <bourgeoisolivier@hotmail.fr>
2024-01-05 19:43:00 +0100

    20-Department-Service - REST API Documentation

--- department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/net/javaguides/departmentservice/DepartmentServiceApplication.java
@@ -3,6 +3,32 @@ package net.javaguides.departmentservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Ramesh",
+						email = "javaguides.net@gmail.com",
+						url = "https://www.javaguides.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.javaguides.net"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department-Service Doc",
+				url = "https://www.javaguides.net"
+		)
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
--- department-service/src/main/java/net/javaguides/departmentservice/dto/DepartmentDto.java
+++ department-service/src/main/java/net/javaguides/departmentservice/dto/DepartmentDto.java
@@ -1,10 +1,14 @@
 package net.javaguides.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(
+		description = "DepartmentDto Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -12,7 +16,19 @@ import lombok.Setter;
 public class DepartmentDto {
 	
 	private Long id;
+	
+	@Schema(
+			description = "Department Name"
+	)
 	private String departmentName;
+	
+	@Schema(
+			description = "Department Description"
+	)
 	private String departmentDescription;
+	
+	@Schema(
+			description = "Department Code"
+	)
 	private String departmentCode;
 }
