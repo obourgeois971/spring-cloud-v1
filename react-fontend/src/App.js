7722653aadf02ff966ecf78afe777e0928bea570
Olivier <bourgeoisolivier@hotmail.fr>
2024-01-05 13:30:53 +0100

    19-Creating React Frontend Microservice

---
+++ react-fontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import EmployeeComponent from "./component/EmployeeComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <EmployeeComponent />
+    </div>
+  );
+}
+
+export default App;
